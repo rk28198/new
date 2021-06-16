3f3d3c283cc315b6ec919deb51407c1fac2ee3cc
unknown <rk28198@gmail.com>
2021-06-16 20:46:30 +0530

    host

--- rfoundation/settings.py
+++ rfoundation/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1g(eqs^jutt4c+g%sp5y^sovrb#d@xsgqfu$0mlelx+g_9k+b)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['rfoundations.herokuapp.com']
+ALLOWED_HOSTS = ['rfoundations.herokuapp.com','rk28198.github.io/new/']
 
 
 # Application definition
